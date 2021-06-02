38734bd8b234c97ebe978b80f777314610a88922
edsnasc <edsnascimento89@gmail.com>
2021-06-02 11:37:38 -0300

    add pj ecommerce

---
+++ categoria.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Categoria</title>
+    <link rel="stylesheet" href="css/style.css" media="screen" title="no title" charset="utf-8">
+</head>
+<body>
+    <header>
+    <div class="center">
+        <h1>Cadastro de categoria</h1>
+        <a href="index.php" terget="self">Voltar</a>
+    </div>
+    </header>
+    <section id="produtos">
+        <form  class="" action="insere-categoria.php" method="post">
+            <label> Descrição </label>
+            <input type="text" name="descricao" required></input>
+            <input type="submit" value="Cadastrar"></input>
+        </form>
+    </section>
+</body>
+</html>
